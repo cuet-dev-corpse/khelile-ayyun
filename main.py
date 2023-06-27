af9080d9363df22841e4324525d2af2a7d922ddf
roundspecs <roundspecs@pm.me>
2023-06-27 21:24:59 +0600

    about command

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os # default module
 from dotenv import load_dotenv
 
 load_dotenv() # load all the variables from the env file
-bot = discord.Bot()
+bot = discord.Bot(intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
@@ -12,5 +12,21 @@ async def on_ready():
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
+
+@bot.slash_command(name = "about", description = "Get to know খেলিলি আইয়ুন")
+async def about(ctx):
+    embed = discord.Embed(
+        title="About খেলিলি আইয়ুন",
+        description="খেলিলি আইয়ুন is a discord bot created to work as a helping hand for CP communities\nCheck out our GitHub repo [here](https://github.com/cuet-dev-corpse/khelile-ayyun)",
+        color=discord.Colour.blurple(),
+    )
+    embed.set_footer(text="Made with 💖 by CUET Dev Corpse") # footers can have icons too
+    await ctx.respond(embed=embed)
+
+@bot.event
+async def on_member_join(member):
+    await member.send(
+        f'Welcome to the server, {member.mention}! Enjoy your stay here.'
+    )
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
