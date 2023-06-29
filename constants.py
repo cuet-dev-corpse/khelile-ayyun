d5bac75f5f06134993037647b8a4e213cc28ea30
roundspecs <roundspecs@pm.me>
2023-06-30 02:29:44 +0600

    add a few slash commands

--- constants.py
+++ constants.py
@@ -1,6 +1,10 @@
+'''
+Contains all the constants in one file so that we can change it without altering the main code
+'''
 from discord import Color
 
 PRIMARY_COLOR = Color.from_rgb(65, 135, 235)
+
 ABOUT_TITLE = "About খেলিলি আইয়ুন"
 ABOUT_DESCRIPTION = (
     "খেলিলি আইয়ুন is a discord bot created to work as a helping hand for CP communities\n"
--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-import discord
-import os  # default module
-from dotenv import load_dotenv
-from discord import ApplicationContext
-from constants import PRIMARY_COLOR, ABOUT_DESCRIPTION, ABOUT_TITLE, ABOUT_FOOTER
+import os
+from math import log2
 
+from discord import (Activity, ActivityType, ApplicationContext, Bot, Embed,
+                     Intents, Member, Option, Status)
+from discord.ext.commands import has_permissions
+from dotenv import load_dotenv
+
+from constants import (ABOUT_DESCRIPTION, ABOUT_FOOTER, ABOUT_TITLE,
+                       PRIMARY_COLOR)
+
+# path of this file
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 # load all the variables from the env file
 load_dotenv(os.path.join(BASEDIR, '.env-var'))
-bot = discord.Bot(
-    intents=discord.Intents.all(),
-    activity=discord.Activity(
-        type=discord.ActivityType.playing,
-        name="Duel against Tourist",
-    ),
-    status=discord.Status.dnd,
+
+# initialize the bot
+bot = Bot(
+    intents=Intents.all(),
+    activity=Activity(type=ActivityType.playing, name="Duel against Tourist"),
+    status=Status.do_not_disturb,
 )
 
 
@@ -22,14 +28,73 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
+@bot.slash_command(name="handle_set", description="Register/change your codeforces handle")
+async def handle_set(ctx: ApplicationContext, handle: Option(input_type=str, description="Codeforces handle", required=True), member: Option(Member, description="Member of this server")):
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
+
+
+@bot.slash_command(name="whois", description="Play duel against an opponent")
+async def whois(ctx: ApplicationContext, member: Option(Member, description="Member of this server")):
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
+
+
 @bot.slash_command(name="duel", description="Play duel against an opponent")
-async def duel(ctx: ApplicationContext):
-    await ctx.respond("The feature is not implemented yet", ephemeral=True)
+async def duel(ctx: ApplicationContext, opponent: Option(Member, description="Member of this server"), rating: Option(int, description="Rating of problem")):
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
+
+
+@bot.slash_command(name="duel_withdraw", description="Play duel against an opponent")
+async def duel_witdraw(ctx: ApplicationContext):
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
+
+
+@bot.slash_command(name="tournament_cancel", description="Cancel the current tournament")
+@has_permissions(moderate_members=True)
+async def tournament_withdraw(ctx: ApplicationContext):
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
+
+
+@bot.slash_command(name="tournament_create", description="Create a new tournament")
+@has_permissions(moderate_members=True)
+async def tournament_create(ctx: ApplicationContext, n: Option(input_type=int, description="Number of players", required=True)):
+    # verification
+    embed = Embed(color=PRIMARY_COLOR)
+    embed.description = (
+        "Number of players should be\n"
+        ":point_right: an integer\n"
+        ":point_right: a power of 2\n"
+        ":point_right: between [8,128]"
+    )
+    if not n.isdecimal():
+        await ctx.respond(embed=embed, ephemeral=True)
+        return
+    n = log2(int(n))
+    if not (n.is_integer() and 3 <= n <= 7):
+        await ctx.respond(embed=embed, ephemeral=True)
+        return
+
+    # TODO
+    embed.description = "The feature is not implemented yet"
+    await ctx.respond(embed=embed, ephemeral=True)
 
 
 @bot.slash_command(name="about", description="Get to know খেলিলি আইয়ুন")
 async def about(ctx: ApplicationContext):
-    embed = discord.Embed(title=ABOUT_TITLE, description=ABOUT_DESCRIPTION, color=PRIMARY_COLOR)
+    embed = Embed(
+        title=ABOUT_TITLE,
+        description=ABOUT_DESCRIPTION,
+        color=PRIMARY_COLOR,
+    )
     embed.set_footer(text=ABOUT_FOOTER)
     await ctx.respond(embed=embed, ephemeral=True)
 
