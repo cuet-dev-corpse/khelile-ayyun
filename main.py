10530b48c0942a70f02334974c6d02ef41fb6ace
roundspecs <roundspecs@pm.me>
2023-06-29 18:41:49 +0600

    Update /about

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os # default module
 from dotenv import load_dotenv
+from discord import ApplicationContext
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env-var')) # load all the variables from the env file
@@ -15,13 +16,18 @@ async def hello(ctx):
     await ctx.respond("Hey!")
 
 @bot.slash_command(name = "about", description = "Get to know খেলিলি আইয়ুন")
-async def about(ctx):
+async def about(ctx: ApplicationContext):
     embed = discord.Embed(
         title="About খেলিলি আইয়ুন",
-        description="খেলিলি আইয়ুন is a discord bot created to work as a helping hand for CP communities\nCheck out our GitHub repo [here](https://github.com/cuet-dev-corpse/khelile-ayyun)",
+        description=(
+            "খেলিলি আইয়ুন is a discord bot created to work as a helping hand for CP communities\n"
+            "The name of the bot was inspired by মেজ্জান হাইলে আইয়ুন\n\n"
+            ":point_right: Type `/help` to see the list of commands\n"
+            ":point_right: Check out our GitHub repo [here](https://github.com/cuet-dev-corpse/khelile-ayyun)\n"
+        ),
         color=discord.Colour.from_rgb(65,135,235),
     )
     embed.set_footer(text="Made with 💖 by CUET Dev Corpse") # footers can have icons too
-    await ctx.respond(embed=embed)
+    await ctx.respond(embed=embed, ephemeral=True)
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
