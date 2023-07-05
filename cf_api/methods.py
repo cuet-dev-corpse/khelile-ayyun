60dfc44a88cda304b7f655fe4a8fef65c030f1cd
roundspecs <roundspecs@pm.me>
2023-07-06 02:53:34 +0600

    Complete methods

--- cf_api/methods.py
+++ cf_api/methods.py
@@ -1,6 +1,6 @@
 from typing import Optional
 import requests
-from objects import Problem, Status
+from objects import Problem, RatingChange, Status, Submission, User
 from exceptions import CFStatusFailed
 
 BASE_URL = "https://codeforces.com/api"
@@ -16,25 +16,59 @@ def problemset_problems(tags: list[str] = [], problemset_name: Optional[str] = N
         params=params,
     ).json()
     if data['status'] == Status.FAILED:
-        raise CFStatusFailed
-    return [Problem(**prob) for prob in data.json()['result']['problems']]
+        raise CFStatusFailed(data['comment'])
+    return [Problem(**problem) for problem in data['result']['problems']]
 
 
-def problemset_reset_status(count, problemset_name):
-    pass
+def problemset_recent_status(count: int) -> list[Submission]:
+    params = {
+        'count': count
+    }
+    data = requests.get(
+        url=BASE_URL + "/problemset.recentStatus",
+        params=params,
+    ).json()
+    if data['status'] == Status.FAILED:
+        raise CFStatusFailed(data['comment'])
+    return [Submission(**submission) for submission in data['result']]
 
 
-def user_info(handles):
-    pass
+def user_info(handles: list[str]) -> list[User]:
+    params = {
+        'handles': ';'.join(handles)
+    }
+    data = requests.get(
+        url=BASE_URL + "/user.info",
+        params=params,
+    ).json()
+    if data['status'] == Status.FAILED:
+        raise CFStatusFailed(data['comment'])
+    return [User(**user) for user in data['result']]
 
 
-def user_rating(handle):
-    pass
+def user_rating(handle: str) -> list[RatingChange]:
+    params = {
+        'handle': handle
+    }
+    data = requests.get(
+        url=BASE_URL + "/user.rating",
+        params=params,
+    ).json()
+    if data['status'] == Status.FAILED:
+        raise CFStatusFailed(data['comment'])
+    return [RatingChange(**rating_change) for rating_change in data['result']]
 
 
-def user_status(handle, _from, count):
-    pass
-
-
-if __name__ == '__main__':
-    print(problemset_problems())
+def user_status(handle: str, _from: int, count: int):
+    params = {
+        'handle': handle,
+        'from': _from,
+        'count': count
+    }
+    data = requests.get(
+        url=BASE_URL + "/user.status",
+        params=params,
+    ).json()
+    if data['status'] == Status.FAILED:
+        raise CFStatusFailed(data['comment'])
+    return [Submission(**submission) for submission in data['result']]
