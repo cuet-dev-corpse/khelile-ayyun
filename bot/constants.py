c5deb7b3ac727ba507a83134757a35d4789eb632
roundspecs <roundspecs@pm.me>
2023-07-11 17:56:37 +0600

    Add more details in the about me

--- bot/constants.py
+++ bot/constants.py
@@ -8,7 +8,7 @@ PRIMARY_COLOR = Color.from_rgb(65, 135, 235)
 ABOUT_TITLE = "About খেলিলি আইয়ুন"
 ABOUT_DESCRIPTION = (
     "খেলিলি আইয়ুন is a discord bot created to work as a helping hand for CP communities\n"
-    "The name of the bot was inspired by মেজ্জান হাইলে আইয়ুন\n\n"
+    "The name of the bot was inspired by the renowned restaurant, মেজ্জান হাইলে আইয়ুন\n\n"
     ":point_right: Check out our GitHub repo [here](https://github.com/cuet-dev-corpse/khelile-ayyun)\n"
     ":construction: The bot is currently under construction. All features may not work properly"
 )
